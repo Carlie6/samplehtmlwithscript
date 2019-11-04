c512ac60f55540fb7365a9c575f846197ee5e788
Carlie6 <57345411+Carlie6@users.noreply.github.com>
2019-11-04 09:33:37 +0100

    Update code.js

--- code.js
+++ code.js
@@ -9,8 +9,8 @@
 <script>
 function myFunction() {
   var x = document.getElementById("demo");
-  x.style.fontSize = "25px"; 
-  x.style.color = "red"; 
+  x.style.fontSize = "35px"; 
+  x.style.color = "blue"; 
 }
 </script>
 
